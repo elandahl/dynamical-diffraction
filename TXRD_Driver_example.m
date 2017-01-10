bbadd9659678da8422ccc2eb4ed86f4aa0656081
elandahl <elandahl@gmail.com>
2017-01-10 02:06:43 -0600

    added driver, other samples

---
+++ TXRD_Driver_example.m
@@ -0,0 +1,31 @@
+% TRXD_Driver_example.m
+% General TRXD master program.  Use this program as a model.
+% By Eric Landahl, 1.9.2017
+%
+clear all; tic; more off;
+
+%% Include subdirectories in path
+addpath('main','include','strain_functions','data');
+
+%% Genreate fresh sample material properties data file
+sampledata; % creates file sample.dat database of material properties
+
+%% Calculate TRXD for Si
+model = 'thermalFilm';
+crystal = 'Si';
+reflection = [0 0 4];
+cut = [0 0 1];
+energy = 10; % in keV
+fluence = 1; % in mJ/cm^2
+angles = 0; % deg. relative, use 0 for default angles
+times = logspace(-1,2.5,20)*1e-9; % in seconds; use 0 for default times
+fprintf('Starting TRXD calculation.\n')
+[A A0 times angles Strain z] = TRXD (model, crystal, reflection, cut, energy, fluence, angles, times);
+
+fprintf('TRXD calculation done, preparing plots.\n')
+%% Make plots, smooth data, calculate centroids
+plot_opts = 'final'; % 'none' = no plots
+ang_res = 2e-4; % angular resultion in degrees FWHM
+[Intensity centroid FWHM] = TRXD_plots (A,A0,times,angles,Strain,z,ang_res,plot_opts);
+
+toc
